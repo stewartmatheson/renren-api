7f70cb406ef60e81f089cf6f29645b2db676a9b7
Stewart Matheson <stewart.matheson@gmail.com>
2012-05-07 13:59:47 +1000

    changed require order as we are not autoloading any more

--- lib/renren-api.rb
+++ lib/renren-api.rb
@@ -1,6 +1,9 @@
+# encoding: UTF-8
+
+require 'renren-api/signature_calculator'
 require 'renren-api/authentication'
 require 'renren-api/http_adapter'
-require 'renren-api/signature_calculator'
+require 'renren-api/error/http_error'
 
 module RenrenAPI
   BASE_URL = "http://api.renren.com/restserver.do"
